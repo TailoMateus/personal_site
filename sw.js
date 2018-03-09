2816333ab7e62fb325bf1ef638cd336101c8d997
Tailo Mateus Gonsalves <tailogonsalves@gmail.com>
2018-03-09 09:38:03 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,25 @@
-/** An empty service worker! */
-self.addEventListener('fetch', function(event) {
-  /** An empty fetch handler! */
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('your-magic-cache').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/blog.html',
+        '/manifest.json',
+        '/images/profile.jpg',
+        '/css/main.css',
+        '/feed.xml',
+        '/js/script.js',
+        '/site.js',
+      ]);
+    })
+  );
 });
 
-self.addEventListener('push', function(event) {
-  event.waitUntil(
-    self.registration.showNotification('Got Push?', {
-      body: 'Push Message received'
-   }));
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
 });

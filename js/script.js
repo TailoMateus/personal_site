b4c4292ab088064ec40875f27c4db1d0b1616f2e
Tailo Mateus Gonsalves <tailogonsalves@gmail.com>
2018-11-21 21:11:31 -0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,21 +1,10 @@
-var il = new Il();
-il.beginFlying();
+setInterval(flyTalents, 1200);
 
-function Il() {
-	var talents = ['code', 'write', 'read', 'think', 'learn', 'run', 'play', 'mathematics', 'science', 'sports', 'podcasts'];
-	var currentIndex = 0;
-	var nextTalent = function() {
-		currentIndex = currentIndex + 1 < talents.length ? currentIndex + 1 : 0;
-	};
-	
-	var talentElement = document.querySelector('#flyup');
-	var flyTalents = function() {
-		nextTalent();
-		talentElement.innerText = talents[currentIndex];
-	};
-	
-	this.beginFlying = function() {
-		setInterval(flyTalents, 1200);
-	};
-	return this;
-}
+var talents = ['code', 'to write', 'to read', 'to learn', 'to run', 'science', 'sports', 'podcasts'];
+var currentIndex = 0;
+
+var talentElement = document.querySelector('#flyup');
+var flyTalents = function() {
+  currentIndex = currentIndex + 1 < talents.length ? currentIndex + 1 : 0;
+  talentElement.innerText = talents[currentIndex];
+};

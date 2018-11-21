f11d892525e7b0d8856c3087f5ea795773fca299
Tailo Mateus Gonsalves <tailogonsalves@gmail.com>
2018-11-21 21:22:55 -0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,9 @@
-setInterval(flyTalents, 1200);
-console.log('Testesss')
-var talents = ['code', 'to write', 'to read', 'to learn', 'to run', 'science', 'sports', 'podcasts'];
-var currentIndex = 0;
+setInterval(flyTalents, 1200)
+let talents = ['code', 'to write', 'to read', 'to learn', 'to run', 'science', 'sports', 'podcasts']
+let currentIndex = 0
 
-var talentElement = document.querySelector('#flyup');
-var flyTalents = function() {
-  currentIndex = currentIndex + 1 < talents.length ? currentIndex + 1 : 0;
-  talentElement.innerText = talents[currentIndex];
-  console.log(talentElement)
-};
+let talentElement = document.querySelector('#flyup')
+function flyTalents() {
+  currentIndex = currentIndex + 1 < talents.length ? currentIndex + 1 : 0
+  talentElement.innerText = talents[currentIndex]
+}

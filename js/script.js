d9d563a69ee763ad95868418abcdc256218dad78
Tailo Mateus Gonsalves <tailogonsalves@gmail.com>
2019-02-10 02:04:48 -0200

    Delete script.js

--- js/script.js
+++
@@ -1,9 +0,0 @@
-setInterval(flyTalents, 1200)
-let talents = ['code', 'to write', 'to read', 'to learn', 'to run', 'science', 'sports', 'podcasts']
-let currentIndex = 0
-
-let talentElement = document.querySelector('#flyup')
-function flyTalents() {
-  currentIndex = currentIndex + 1 < talents.length ? currentIndex + 1 : 0
-  talentElement.innerText = talents[currentIndex]
-}
